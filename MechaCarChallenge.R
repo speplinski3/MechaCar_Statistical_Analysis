c823ae75ca5f0f358a6a804dec31fbbd70bbd2cf
Shanika Peplinski <peplinski.shanika@gmail.com>
2022-09-10 14:39:40 -0700

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,26 @@ total_summary_df <- summarize(coil_df, mean(PSI), median(PSI), var(PSI), sd(PSI)
 lot_summary_df <- group_by(coil_df, Manufacturing_Lot)
 
 group_by_df <- summarize(lot_summary_df, mean(PSI), median(PSI), var(PSI), sd(PSI))
+# Boxplot All
+plt1 <- ggplot(coil_df, aes (y=PSI))
+plt1 + geom_boxplot()
+
+#Boxplot Individual Lot
+plt2 <- ggplot(coil_df, aes(x=Manufacturing_Lot, y=PSI))
+plt2 + geom_boxplot()
+
+# Deliverable 3
+
+# Summary of the t-test results across all manufacturing lots
+t.test(coil_df$PSI,mu=1500)
+
+# T-tests for individual lots
+summary(coil_df$PSI)
+# Creating Subsets
+lot1 <- subset(coil_df, Manufacturing_Lot=="Lot1")
+lot2 <- subset(coil_df, Manufacturing_Lot=="Lot2")
+lot3 <- subset(coil_df, Manufacturing_Lot=="Lot3")
+#T-Tests Individual
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
